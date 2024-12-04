39c7017e97ae1202f10e309d5c82448290331919
Moonsh1ne <moonsh1ne.vlad@yandex.ru>
2024-12-04 15:59:54 +0300

    Some fix, update, upgrade

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,54 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function(event) {
     event.preventDefault(); // Предотвращение отправки формы
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
 
     // Симуляция успешной отправки данных с использованием LocalStorage
-    localStorage.setItem('contactFormData', JSON.stringify({ name, email, message }));
+    localStorage.setItem(
+      "contactFormData",
+      JSON.stringify({ name, email, message })
+    );
 
-    document.getElementById('result').innerHTML = 'Thank you for contacting us! Your message has been saved locally.';
-    document.getElementById('contactForm').reset(); // Сброс формы
+    document.getElementById("result").innerHTML =
+      "Thank you for contacting us! Your message has been saved locally.";
+    document.getElementById("contactForm").reset(); // Сброс формы
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll('.btn--bounce-left, .btn--bounce-right');
+    const works = document.querySelectorAll('.works__card');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio === 1) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 1.0 });
+
+  const observerWorks = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add("visible");
+        observerWorks.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.5 });
+
+  buttons.forEach(button => {
+    observer.observe(button);
+  });
+
+    works.forEach(work => {
+        observerWorks.observe(work);
+    });
 });
 
-
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,11 +4,25 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
+	// Функция для открытия и закрытия навигации
+	const toggleNav = () => {
+		const isOpen = nav.classList.toggle('mobile-nav--open');
 		menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
+		navBtn.setAttribute('aria-expanded', isOpen);
 	};
+
+	// Обработчик для кнопки навигации
+	navBtn.addEventListener('click', (event) => {
+		toggleNav();
+	});
+
+	// Обработчик для клика вне меню и по элементам меню
+	nav.addEventListener('click', (event) => {
+		if (nav.classList.contains('mobile-nav--open')) {
+			toggleNav();
+		}
+	});
 }
 
 export default mobileNav;
